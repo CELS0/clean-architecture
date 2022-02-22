b8177a3ed54d8793fbf584070c7a5822c14e4e85
CELS0 <celsojr.300@gmail.com>
2022-02-22 17:28:57 -0300

    test: ensure return codeStatus 400 if no email

--- src/reprentation/controllers/SignUpController.spec.ts
+++ src/reprentation/controllers/SignUpController.spec.ts
@@ -4,7 +4,6 @@ describe('SignUpController.spec', () => {
     test('Should return 400 if no name is provided', () => {
         const sut = new SignUpController();
         const httpRequest: IHttpRequest = {
-            name: 'any_name',
             email: 'any_email',
             password: 'any_password',
             passwordConfirmation: 'any_password_confirmation'
@@ -13,5 +12,18 @@ describe('SignUpController.spec', () => {
 
         expect(httpReponse.statusCode).toBe(400);
         expect(httpReponse.body).toEqual(new Error('Missing param: name'));
+    });
+
+    test('Should return 400 if no email is provided', () => {
+        const sut = new SignUpController();
+        const httpRequest: IHttpRequest = {
+            name: 'any_name',
+            password: 'any_password',
+            passwordConfirmation: 'any_password_confirmation'
+        }
+        const httpReponse = sut.handle(httpRequest);
+
+        expect(httpReponse.statusCode).toBe(400);
+        expect(httpReponse.body).toEqual(new Error('Missing param: email'));
     });
 });
