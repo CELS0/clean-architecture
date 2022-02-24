cc56d7de2b26604c5c2a5883ba056d180a03aeaa
CELS0 <celsojr.300@gmail.com>
2022-02-23 21:48:31 -0300

    feat

--- src/reprentation/controllers/SignUpController.spec.ts
+++ src/reprentation/controllers/SignUpController.spec.ts
@@ -1,8 +1,15 @@
-import { MissingParamError } from "../erros/MissingParamErros";
+import { InvalidParamError, MissingParamError } from "../erros";
+import { IEmailValidator } from "../protocols/emailValidator";
 import { SignUpController } from "./SignUpController";
 
 const makeSut = (): SignUpController => {
-    return new SignUpController();
+    class MailValidatorStub implements IEmailValidator {
+        isValid(email: string): boolean {
+            return false;
+        }
+    }
+    const mailValidatorStub = new MailValidatorStub();
+    return new SignUpController(mailValidatorStub);
 };
 
 describe('SignUpController.spec', () => {
@@ -68,5 +75,23 @@ describe('SignUpController.spec', () => {
 
         expect(httpReponse.statusCode).toBe(400);
         expect(httpReponse.body).toEqual(new MissingParamError('passwordConfirmation'));
+    });
+
+    test('Should return 400 if an invalid email is provided', () => {
+        const sut = makeSut();
+
+        const httpRequest = {
+            body: {
+                email: 'invalid_email',
+                name: 'any_name',
+                password: 'any_password',
+                passwordConfirmation: 'any_password_confirmation'
+            }
+        }
+
+        const httpReponse = sut.handle(httpRequest);
+
+        expect(httpReponse.statusCode).toBe(400);
+        expect(httpReponse.body).toEqual(new InvalidParamError('email'));
     });
 });
